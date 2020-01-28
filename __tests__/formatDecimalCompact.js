bb9a7b2f97c9e5d4a723d04dd78dc052b628b45f
risan <risanbagja@gmail.com>
2020-01-28 22:15:12 +0700

    formatDecimalCompact function

---
+++ __tests__/formatDecimalCompact.js
@@ -0,0 +1,35 @@
+/* global expect:false, test:false */
+import { formatDecimalCompact } from '../src/index';
+
+test('it can format an integer type', () => {
+  expect(formatDecimalCompact(100)).toBe('100.00');
+  expect(formatDecimalCompact(1000)).toBe('1.00K');
+  expect(formatDecimalCompact(1250)).toBe('1.25K');
+  expect(formatDecimalCompact(-1250)).toBe('-1.25K');
+  expect(formatDecimalCompact(1250000)).toBe('1.25M');
+  expect(formatDecimalCompact(1250000000)).toBe('1.25B');
+});
+
+test('it can format a float type', () => {
+  expect(formatDecimalCompact(1250.0)).toBe('1.25K');
+  expect(formatDecimalCompact(-1250.0)).toBe('-1.25K');
+});
+
+test('it can format a number with string type', () => {
+  expect(formatDecimalCompact('1250.0')).toBe('1.25K');
+  expect(formatDecimalCompact('-1250.0')).toBe('-1.25K');
+});
+
+test('it returns null if it cannot be parsed', () => {
+  expect(formatDecimalCompact('foo')).toBeNull();
+  expect(formatDecimalCompact([])).toBeNull();
+  expect(formatDecimalCompact({})).toBeNull();
+});
+
+test('it can accept a custom fraction digits', () => {
+  expect(formatDecimalCompact(1257, 0)).toBe('1K');
+  expect(formatDecimalCompact(1257, 1)).toBe('1.3K');
+  expect(formatDecimalCompact(1257, 2)).toBe('1.26K');
+  expect(formatDecimalCompact(1257, 3)).toBe('1.257K');
+  expect(formatDecimalCompact(1257, 4)).toBe('1.2570K');
+});
--- src/index.js
+++ src/index.js
@@ -99,6 +99,47 @@ export const formatDecimal = (
   }).format(valueParsed);
 };
 
+const parseNumberCompact = value => {
+  const valueParsed = parseNumber(value);
+
+  if (valueParsed === null) {
+    return { value: null, symbol: null };
+  }
+
+  const groups = [
+    { divider: 1, symbol: '' },
+    { divider: 1e3, symbol: 'K' },
+    { divider: 1e6, symbol: 'M' },
+    { divider: 1e9, symbol: 'B' },
+  ];
+
+  const matchedGroup = groups.reduce((matched, group) => {
+    if (Math.abs(valueParsed) >= group.divider) {
+      return group;
+    }
+
+    return matched;
+  }, groups[0]);
+
+  const valueCompact = valueParsed / matchedGroup.divider;
+
+  return { value: valueCompact, symbol: matchedGroup.symbol };
+};
+
+export const formatDecimalCompact = (
+  value,
+  fractionDigits = 2,
+  options = {}
+) => {
+  const { value: valueCompact, symbol } = parseNumberCompact(value);
+
+  if (valueCompact === null) {
+    return null;
+  }
+
+  return formatDecimal(valueCompact, fractionDigits, options) + symbol;
+};
+
 export const formatCurrency = (
   value,
   fractionDigits = 2,
