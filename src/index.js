a5bca6f7dda5ae749c7ceca8e1e2f782d30fad6c
risan <risanbagja@gmail.com>
2020-01-28 20:56:59 +0700

    Rename val to value

--- src/index.js
+++ src/index.js
@@ -1,62 +1,62 @@
 import isPlainObject from 'lodash/isPlainObject';
 
-export const isArray = val => Array.isArray(val);
+export const isArray = value => Array.isArray(value);
 
-export const isBoolean = val => typeof val === 'boolean';
+export const isBoolean = value => typeof value === 'boolean';
 
-export const isString = val => typeof val === 'string';
+export const isString = value => typeof value === 'string';
 
-export const isNumber = val =>
-  typeof val === 'number' && !Number.isNaN(val) && Number.isFinite(val);
+export const isNumber = value =>
+  typeof value === 'number' && !Number.isNaN(value) && Number.isFinite(value);
 
-export const isDate = val =>
-  val instanceof Date && val.toString() !== 'Invalid Date';
+export const isDate = value =>
+  value instanceof Date && value.toString() !== 'Invalid Date';
 
-export const isMap = val => val instanceof Map;
+export const isMap = value => value instanceof Map;
 
-export const isSet = val => val instanceof Set;
+export const isSet = value => value instanceof Set;
 
-export const isObject = val => {
+export const isObject = value => {
   if (
-    val === null ||
-    isArray(val) ||
-    val instanceof Date ||
-    isSet(val) ||
-    isMap(val)
+    value === null ||
+    isArray(value) ||
+    value instanceof Date ||
+    isSet(value) ||
+    isMap(value)
   ) {
     return false;
   }
 
-  return typeof val === 'object';
+  return typeof value === 'object';
 };
 
-export const isEmpty = val => {
-  if (val === undefined || val === null || Number.isNaN(val)) {
+export const isEmpty = value => {
+  if (value === undefined || value === null || Number.isNaN(value)) {
     return true;
   }
 
-  if (isBoolean(val) || isNumber(val)) {
+  if (isBoolean(value) || isNumber(value)) {
     return false;
   }
 
-  if (isString(val)) {
-    return val.trim().length === 0;
+  if (isString(value)) {
+    return value.trim().length === 0;
   }
 
-  if (isArray(val)) {
-    return val.length === 0;
+  if (isArray(value)) {
+    return value.length === 0;
   }
 
-  if (isSet(val) || isMap(val)) {
-    return val.size === 0;
+  if (isSet(value) || isMap(value)) {
+    return value.size === 0;
   }
 
-  if (val instanceof Date) {
-    return !isDate(val);
+  if (value instanceof Date) {
+    return !isDate(value);
   }
 
-  if (isPlainObject(val)) {
-    return Object.keys(val).length === 0;
+  if (isPlainObject(value)) {
+    return Object.keys(value).length === 0;
   }
 
   return false;
